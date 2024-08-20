12e1831caee3a545ca752b7c8e1c889918a72dec
Raúl Granados <raul.granados12@gmail.com>
2024-08-20 15:53:02 -0600

    renaming file

--- src/flows/index.ts
+++ src/flows/index.ts
@@ -3,8 +3,8 @@ import { welcomeFlow } from "./welcomeFlow.flow";
 import { mediaFlow } from "./mediaFlow";
 import { voiceNoteFlow } from "./voiceNote.flow";
 import { groupsID } from "./groupsID.flow";
-import { imageFlow } from "./imageFlow.flow";
-import { resizeFlow } from "./imageFlow.flow";
+import { imageFlow } from "./alertsFlow.flow";
+import { resizeFlow } from "./alertsFlow.flow";
 import { analyseImageFlow } from "./analyseImageFlow";
 
 export const flow = createFlow([
