7bd5ca2d4e790111be1372180fde475ca90b4ff0
MJ <mariajmr1202@gmail.com>
2024-09-02 20:16:47 -0400

    Add checkAndCleanLatestLog function

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -10,12 +10,28 @@ function getFormattedDate (): string {
 function writeFile(logMessage): void {
     const logDir = process.argv.includes('--artifacts') ? '/var/log/vertex/bot/' : '/tmp/vertex/bot/';
     const filename = `log_bot.${getFormattedDate()}`;
+    const latestFilename = path.join(logDir, 'log_bot.latest');
 
     if (!fs.existsSync(logDir)) {
         fs.mkdirSync(logDir, { recursive: true });
     }
 
     const logFile = fs.createWriteStream(path.join(logDir, filename), { flags: 'a' });
+    logFile.write(logMessage); 
+    checkAndCleanLatestLog(latestFilename, logMessage);
+}
+
+function checkAndCleanLatestLog(latestFilename, logMessage) {
+    if (fs.existsSync(latestFilename)) {
+        const stats = fs.statSync(latestFilename);
+        const lastModifiedHour = new Date(stats.mtime).getHours(); 
+        const currentHour = new Date().getHours(); 
+
+        if (currentHour !== lastModifiedHour) {
+           fs.writeFileSync(latestFilename, ''); // Clean file
+        }
+    }
+    const logFile = fs.createWriteStream(latestFilename, { flags: 'a' });
     logFile.write(logMessage); 
 }
 
