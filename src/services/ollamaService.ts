40fe3528e02730b40f2f294c4c58d702be79a604
Sergio <sergiorobayorr@gmail.com>
2024-09-05 09:08:24 -0600

    ollama service

--- src/services/ollamaService.ts
+++ src/services/ollamaService.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { Session } from "../models/Session";
 import { setupLogger } from "../utils/logger";
+import { Ollama } from "@langchain/ollama";
 
 const OLLAMA_API_URL = process.env.OLLAMA_API_URL || "http://localhost:11434";
 const OLLAMA_API_URL_GENERATE = `${OLLAMA_API_URL}/api/generate`;
@@ -8,6 +9,15 @@ const OLLAMA_API_URL_CHAT = `${OLLAMA_API_URL}/api/chat`;
 const OLLAMA_API_URL_EMBEDDING = `${OLLAMA_API_URL}/api/embeddings`;
 const MODEL = process.env.MODEL || "llama3.1";
 const EMBEDDING_MODEL = process.env.EMBEDDING_MODEL || "nomic-embed-text";
+
+export const llm = new Ollama({
+  baseUrl: OLLAMA_API_URL,
+  model: MODEL,
+  temperature: 0.3,
+  topK: 20,
+  topP: 0.45,
+  numCtx: 30720,
+});
 
 setupLogger();
 
