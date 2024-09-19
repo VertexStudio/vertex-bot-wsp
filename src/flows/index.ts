d9782a6c80b231d12b342777e809ecfaa0dda905
Francisco <contact.frann@gmail.com>
2024-09-19 19:25:13 -0300

    fix's

--- src/flows/index.ts
+++ src/flows/index.ts
@@ -1,14 +1,12 @@
 import { createFlow } from "@builderbot/bot";
 import { welcomeFlow } from "./welcomeFlow.flow";
-import { groupsID } from "./groupsID.flow";
 import { alertsFlow } from "./alertsFlow.flow";
 import { analyseImageFlow } from "./analyseImageFlow";
 import { languageFlow } from './languageFlow'
 
 export const flow = createFlow([
-  //welcomeFlow,
-  groupsID,
-  //alertsFlow,
+  welcomeFlow,
+  alertsFlow,
   languageFlow,
-  //analyseImageFlow,
+  analyseImageFlow,
 ]);
