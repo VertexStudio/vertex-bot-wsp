292ff992ac209bcad6129a2aa13a9f35c26bbe3d
Sergio <sergiorobayorr@gmail.com>
2024-09-19 18:54:28 -0500

    modify role and model task queries

--- src/flows/analyseImageFlow.ts
+++ src/flows/analyseImageFlow.ts
@@ -173,8 +173,8 @@ async function updateDatabaseWithModelTask(
   model_task: ImageAnalysisType
 ): Promise<void> {
   const updateQuery = `
-    LET $linkedModelTask = (SELECT (->camera_tasks->task->task_model_tasks.out)[0] AS model_task FROM $camera)[0].model_task;
-    UPDATE $linkedModelTask SET task = $model_task;
+    LET $linkedTask = (SELECT (->camera_tasks.out)[0] AS task FROM $camera)[0].task;
+    UPDATE $linkedTask SET model_task = $model_task;
   `;
 
   const query = await db.query(updateQuery, {
