85e3a7d2f682365da090579245c737786b5fa12b
Francisco <contact.frann@gmail.com>
2024-10-18 19:36:38 -0300

    ctx changes in response

--- src/services/responseService.ts
+++ src/services/responseService.ts
@@ -4,9 +4,9 @@ export async function sendResponse(provider: any, ctx: any, content: string) {
   let messageText = content;
   let mentions: string[] = [];
 
-  if (ctx.key.participant) {
-    messageText = `@${ctx.key.participant.split("@")[0]} ${messageText}`;
-    mentions = [ctx.key.participant];
+  if (ctx.messageCtx.update.message.from.username) {
+    messageText = `@${ctx.messageCtx.update.message.from.username} ${messageText}`;
+    mentions = [ctx.messageCtx.update.message.from.username];
   }
 
   await sendMessage(provider, ctx.from, messageText, mentions, ctx);
