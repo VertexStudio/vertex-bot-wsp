8f71f60b38b84d08830e0b497ead62f5cac7c898
Francisco <contact.frann@gmail.com>
2024-10-25 15:35:54 -0300

    messageServie changed to telegram plugin

--- src/services/messageService.ts
+++ src/services/messageService.ts
@@ -1,7 +1,8 @@
-import { BaileysProvider } from "@builderbot/provider-baileys";
+//import { BaileysProvider } from "@builderbot/provider-baileys";
+import { TelegramProvider } from '@builderbot-plugins/telegram'
 
 export async function sendMessage(
-  provider: BaileysProvider,
+  provider: TelegramProvider,
   remoteJid: string,
   messageText: string,
   mentions: string[] = [],
@@ -9,10 +10,10 @@ export async function sendMessage(
   retryCount: number = 0
 ) {
   try {
-    await provider.vendor.sendMessage(
+    await provider.vendor.telegram.sendMessage(
       remoteJid,
-      { text: messageText, mentions },
-      quotedMessage ? { quoted: quotedMessage } : undefined
+      { text: messageText, mentions } as any,
+      quotedMessage ? { quoted: quotedMessage } as any : undefined
     );
   } catch (error) {
     if (error.message === "rate-overlimit" && retryCount < 3) {
